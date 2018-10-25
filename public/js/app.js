58dad398b20ff6b991b24319cde4f84b9c69c359
Kyle Meserve <KyleMeserve@Kyles-MacBook-Air.local>
2018-10-24 21:02:53 -0400

    Created addResort function to app.js - tested and is working

--- public/js/app.js
+++ public/js/app.js
@@ -2,4 +2,20 @@ const app = angular.module('DisneyApp', []);
 
 app.controller('DisneyController', ['$http', function($http){
     this.hello = 'Hi';
+
+    this.addResort = ()=>{
+        $http({
+            method: 'POST',
+            url: '/resorts',
+            data: {
+                title: this.newTitle,
+                url: this.newUrl,
+                img: this.newImg
+            }
+        }).then(response=>{
+            console.log(response);
+        }, error=>{
+            console.log(error);
+        })
+    }
 }]);
